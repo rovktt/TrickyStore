087e5f4df46838391ea95fef38809823f6c22ed2
rovktt <31450489+rovktt@users.noreply.github.com>
2024-08-13 07:02:42 +0800

    Update service.sh

--- module/template/service.sh
+++ module/template/service.sh
@@ -1,3 +1,7 @@
+resetprop ro.boot.flash.locked 1
+resetprop ro.boot.verifiedbootstate green
+resetprop ro.secureboot.lockstate locked
+resetprop ro.boot.vbmeta.device_state locked
 DEBUG=@DEBUG@
 
 MODDIR=${0%/*}
